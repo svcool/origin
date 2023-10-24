ea5880f23d1dbb29e7bb28322ddc8059c1be20b5
SergeyYar <sergyar@list.ru>
2023-10-25 09:26:55 +1000

    Revision Task1

--- Lesson2/Task1/task1.cpp
+++ Lesson2/Task1/task1.cpp
@@ -13,7 +13,7 @@ enum class months {
 	октябрь,
 	ноябрь,
 	декабрь,
-};
+}; 
 
 int main() {
 	setlocale(LC_ALL, "Russian");
@@ -23,20 +23,20 @@ int main() {
 		std::cout << "Введите номер месяца: ";
 		std::cin >> number;
 		months mon = static_cast<months>(number);
-		switch (static_cast<int>(mon)) {
-		case 0: std::cout << "До свидания" << std::endl; break;
-		case 1: std::cout << "январь" << std::endl; break;
-		case 2: std::cout << "февраль" << std::endl; break;
-		case 3: std::cout << "март" << std::endl; break;
-		case 4: std::cout << "апрель" << std::endl; break;
-		case 5: std::cout << "май" << std::endl; break;
-		case 6: std::cout << "июнь" << std::endl; break;
-		case 7: std::cout << "июль" << std::endl; break;
-		case 8: std::cout << "август" << std::endl; break;
-		case 9: std::cout << "сентябрь" << std::endl; break;
-		case 10: std::cout << "октябрь" << std::endl; break;
-		case 11: std::cout << "ноябрь" << std::endl; break;
-		case 12: std::cout << "декабрь" << std::endl; break;
+		if (number == 0) { std::cout << "До свидания" << std::endl; break; }
+		switch (mon) {
+		case months::январь: std::cout << "январь" << std::endl; break;
+		case months::февраль: std::cout << "февраль" << std::endl; break;
+		case months::март: std::cout << "март" << std::endl; break;
+		case months::апрель: std::cout << "апрель" << std::endl; break;
+		case months::май: std::cout << "май" << std::endl; break;
+		case months::июнь: std::cout << "июнь" << std::endl; break;
+		case months::июль: std::cout << "июль" << std::endl; break;
+		case months::август: std::cout << "август" << std::endl; break;
+		case months::сентябрь: std::cout << "сентябрь" << std::endl; break;
+		case months::октябрь: std::cout << "октябрь" << std::endl; break;
+		case months::ноябрь: std::cout << "ноябрь" << std::endl; break;
+		case months::декабрь: std::cout << "декабрь" << std::endl; break;
 		default: std::cout << "Неправильный номер!" << std::endl; break;
 		}
 	} while (number != 0);
