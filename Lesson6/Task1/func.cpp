c1615984db8bcd603dab1306bb602b7f45fd48a0
SergeyYar <sergyar@list.ru>
2023-11-10 16:46:35 +1000

    Lesson6

--- Lesson6/Task1/func.cpp
+++ Lesson6/Task1/func.cpp
@@ -1,21 +1,19 @@
-#include <iostream>
 double pow(double x, double y) {
 	double result{1};
 	for (int i = 1; i <= y; i++) {
 		result = result * x;
 	}
-	return std::cout << x << " в степени " << y << "=" << result << std::endl;;
+	return result;
 }
-
 double add(double x, double y) {
-	return std::cout << "Сумма" << x << " и " << y << "=" << x + y << std::endl;
+	return x + y;
 };
 double subtraction(double x, double y) {
-	return std::cout  << "Разность" << x << " и " << y << "=" << x - y << std::endl;
+	return x - y;
 };
 double multiplication(double x, double y) {
-	return std::cout << "Произведение" << x << " и " << y << "=" << x * y << std::endl;;
+	return x * y;
 };
 double division(double x, double y) {
-	return std::cout << "Деление" << x << " и " << y << "=" << x / y << std::endl;;
+	return x / y;
 }
