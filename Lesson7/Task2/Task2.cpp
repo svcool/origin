c8ac2076159501715a0f95dc6d5209d5f48aee90
Myrep <sergyar@list.ru>
2023-11-13 00:24:10 +1000

    Lesson7,

---
+++ Lesson7/Task2/Task2.cpp
@@ -0,0 +1,12 @@
+﻿#define MAX(a, b) ((a) \
+ >= (b) ? (a) : (b)) // Определили макрос MAX
+
+int main(int argc, char** argv)
+{
+	int a = 6;
+	int b = 5;
+	int c = 2;
+	std::cout << SUB(a, b) << std::endl;
+	std::cout << SUB(a, b) * c << std::endl;
+	std::cout << SUB(a, b + c) * c << std::endl;
+}
