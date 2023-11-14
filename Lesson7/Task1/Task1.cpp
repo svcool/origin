970898ed42bf3f4326f3ad19d9c2499fedd38187
SergeyYar <sergyar@list.ru>
2023-11-14 11:34:06 +1000

    Revert "слияниеL7,T1"
    This reverts commit 5bd8f3c403506a0635828c8f710cc60521d3a3e5.

--- Lesson7/Task1/Task1.cpp
+++ Lesson7/Task1/Task1.cpp
@@ -1,1 +1,18 @@
-﻿
+﻿#include "iostream"
+#include "func.h"
+#define MODE 1 // Определили идентификатор MODE
+#ifndef MODE
+#error Определите константу MODE
+#endif
+
+int main(int argc, char** argv) {
+setlocale(LC_ALL, "Russian");
+#if MODE == 0
+	std::cout << "Работаю в режиме тренировки";
+#elif MODE == 1
+std::cout << "Работаею в боевом режиме" << std::endl;
+add();
+#else
+	std::cout << "Неизвестный режим. Завершение работы";
+#endif
+}
