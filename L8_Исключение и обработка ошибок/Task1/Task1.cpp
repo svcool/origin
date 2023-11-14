e58449f08a60f6c27b0a92c74ad998f78f3772cc
SergeyYar <sergyar@list.ru>
2023-11-14 16:32:12 +1000

    Lesson4_T1_T2

---
+++ L8_Исключение и обработка ошибок/Task1/Task1.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+
+int function(std::string, int);
+int main() {
+	setlocale(LC_ALL, "Russian");
+	system("chcp 1251");
+
+
+}
+int function(std::string str, int forbidden_length) {
+
+}
