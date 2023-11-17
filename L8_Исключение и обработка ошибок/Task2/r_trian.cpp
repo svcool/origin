1e506bba8265277058db33d2963e16942918c3b9
SergeyYar <sergyar@list.ru>
2023-11-17 16:50:25 +1000

    1

--- L8_Исключение и обработка ошибок/Task2/r_trian.cpp
+++ L8_Исключение и обработка ошибок/Task2/r_trian.cpp
@@ -2,4 +2,9 @@
 
 right_triangle::right_triangle(double a, double b, double c, double A, double B) : triangle(a, b, c, A, B, 90) {
 		name = "Прямоугольный треугольник";
+		if (С != 90)
+		{
+			throw Except("Угол С не равен 90 градусов");
+		}
+
 	}
