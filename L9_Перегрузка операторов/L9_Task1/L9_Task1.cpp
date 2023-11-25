73f4bdc69350b81da08040d298929721cfa340ee
Myrep <sergyar@list.ru>
2023-11-25 17:20:33 +1000

    L9, final

--- L9_Перегрузка операторов/L9_Task1/L9_Task1.cpp
+++ L9_Перегрузка операторов/L9_Task1/L9_Task1.cpp
@@ -1,1 +1,43 @@
-﻿
+﻿#include <iostream>
+
+class Fraction
+{
+private:
+	int numerator_;
+	int denominator_;
+
+public:
+	Fraction(int numerator, int denominator)
+	{
+		numerator_ = numerator;
+		denominator_ = denominator;
+	}
+	
+	
+
+	bool operator==(Fraction other) { return (numerator_ / denominator_) == (other.numerator_ / other.denominator_) ? 1 : 0;}
+	bool operator!=(Fraction other) { return !(*this == other); }
+	bool operator>(Fraction other) { return (numerator_ / denominator_) > (other.numerator_ / other.denominator_) ? 1 : 0; }
+	bool operator<(Fraction other) { return other > *this; }
+	bool operator>=(Fraction other) { return !(*this < other); }
+	bool operator<=(Fraction other) { return !(*this > other); }
+
+};
+
+int main()
+{
+	Fraction f1(4, 3);
+	Fraction f2(6, 11);
+
+	std::cout << "f1" << ((f1 == f2) ? " == " : " not == ") << "f2" << '\n';
+	std::cout << "f1" << ((f1 != f2) ? " != " : " not != ") << "f2" << '\n';
+	std::cout << "f1" << ((f1 < f2) ? " < " : " not < ") << "f2" << '\n';
+	std::cout << "f1" << ((f1 > f2) ? " > " : " not > ") << "f2" << '\n';
+	std::cout << "f1" << ((f1 <= f2) ? " <= " : " not <= ") << "f2" << '\n';
+	std::cout << "f1" << ((f1 >= f2) ? " >= " : " not >= ") << "f2" << '\n';
+
+
+
+
+	return 0;
+}
