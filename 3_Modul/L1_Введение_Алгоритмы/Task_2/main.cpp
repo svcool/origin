3f3ba0c724d06a538e30c7670a041f1b0f0291b5
Myrep <sergyar@list.ru>
2023-12-20 23:39:59 +1000

    1

--- 3_Modul/L1_Введение_Алгоритмы/Task_2/main.cpp
+++ 3_Modul/L1_Введение_Алгоритмы/Task_2/main.cpp
@@ -1,14 +1,18 @@
 ﻿#include <iostream>
 int function(int* arr, const int size, int number) {
-	int result;
+	int result{};
 	int left = 0;
 	int right = size - 1;
-	while (left < right) {
-		int middle = (left + right / 2);
-if (arr[middle]== number) result = middle;
-		else if (arr[middle] > number) right = middle - 1;
-		else if (arr[middle] < number) left = middle + 1;
-		if (arr[left] == number) result = arr[left];
+	while (true) {
+		int middle = (left + right )/ 2;
+		if (arr[middle] > number) {
+			result = size - middle;
+			right = middle - 1; 
+		}
+		else if (arr[middle] <= number) {
+			left = middle + 1;
+		}
+		if (left > right) return result;
 	}
 	return result;
 }
@@ -18,9 +22,9 @@ int main() {
 	int number = 0;
 	const int size = 9;
 	int arr[9] = {14, 16, 19, 32, 32, 32, 56, 69, 72};
-	std::cout << "Введите точку отсчёта:"; std::cin >> number;
-
-	std::cout << "Количество элементов в массиве больших, чем 15:" << function(arr, size, number);
-	
+	while (true) {
+		std::cout << "Введите точку отсчёта: "; std::cin >> number;
+		std::cout << "Количество элементов в массиве больших, чем " << number << ": " << function(arr, size, number) << std::endl;
+	}
 
 }
