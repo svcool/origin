1f5f26ededaf56fff4d1909756df7135ff281ae1
SergeyYar <sergyar@list.ru>
2023-12-22 16:46:24 +1000

    1

--- 3_Modul/L2_Динамическое_програмирование/Task1/main.cpp
+++ 3_Modul/L2_Динамическое_програмирование/Task1/main.cpp
@@ -1,20 +1,16 @@
 ﻿#include <iostream>
-unsigned int fib(int n)
+int fib(int n)
 {
-    return  n < 2 ? n : fib(n - 1) + fib(n - 2);
+    if (n <= 1)
+        return n;
+    return fib(n - 1) + fib(n - 2);
 }
 
 int main()
 {
-    setlocale(LC_ALL, "Russian");
-    unsigned int n = 0;
-    int i = 0;
-    std::cout << "Введите число: "; 
-    std::cin >> n;
-    while (i < n) {
-        std::cout << fib(i) << " ";
-        ++i;
-    }
+    int n = 20;
+    std::cout << fib(n);
+    getchar();
     return 0;
 //https://translated.turbopages.org/proxy_u/en-ru.ru.547d0e0c-6585303f-e9e29000-74722d776562/https/www.geeksforgeeks.org/cpp-program-for-fibonacci-numbers/
 }
