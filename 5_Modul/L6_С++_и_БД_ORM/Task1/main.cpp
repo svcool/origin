cd73c15691d7f2c4e9fc27027db17f5a1d22e001
SergeyYar <sergyar@list.ru>
2024-04-01 16:47:56 +1000

    1

--- 5_Modul/L6_С++_и_БД_ORM/Task1/main.cpp
+++ 5_Modul/L6_С++_и_БД_ORM/Task1/main.cpp
@@ -4,21 +4,81 @@
 #include <Windows.h>
 using namespace std;
 #pragma execution_character_set("utf-8")
+class Book;
+class Shop;
 
-class user {
+
+class Publisher {
 public:
 	string name = "";
-	string phone = "";
-	int karma = 0;
+	Wt::Dbo::collection<Wt::Dbo::ptr<Book>> books;
+
 	template<typename Action>
 	void persist(Action& a) {
 		Wt::Dbo::field(a, name, "name");
-		Wt::Dbo::field(a, phone, "phone");
-		Wt::Dbo::field(a, karma, "karma");
+		Wt::Dbo::hasMany(a, books, Wt::Dbo::ManyToOne, "publisher")
 	}
-
-
 };
+
+	class Book {
+	public:
+		string title = "";
+		Wt::Dbo::ptr<Publisher> publisher;
+		Wt::Dbo::collection<Wt::Dbo::ptr<Stock>> stocks;
+
+		template<typename Action>
+		void persist(Action& a) {
+			Wt::Dbo::field(a, title, "title");
+			Wt::Dbo::belongsTo(a, publishers, "publisher");  //дописывает _id в таблице
+			Wt::Dbo::hasMany(a, stocks, Wt::Dbo::ManyToOne, "book")
+		}
+	};
+
+	class Stock {
+	public:
+		int count{};
+		Wt::Dbo::ptr<Shop> shops;
+		Wt::Dbo::ptr<Book> books;
+		Wt::Dbo::collection<Wt::Dbo::ptr<Sale>> sales;
+
+		template<typename Action>
+		void persist(Action& a) {
+			Wt::Dbo::field(a, count, "count");
+			Wt::Dbo::belongsTo(a, Shop, "shop");
+			Wt::Dbo::belongsTo(a, book, "book");
+			Wt::Dbo::hasMany(a, sales, Wt::Dbo::ManyToOne, "stock");
+		}
+	};
+
+	class Shop{
+	public:
+		string name = "";
+		Wt::Dbo::collection<Wt::Dbo::ptr<Stock>> stocks;
+
+		template<typename Action>
+		void persist(Action& a) {
+			Wt::Dbo::field(a, name, "name");
+			Wt::Dbo::hasMany(a, stocks, Wt::Dbo::ManyToOne, "shop");
+		}
+	};
+
+	class Sale {
+	public:
+		string name = "";
+		int price{};
+		string date_sale="";
+		int count{};
+		Wt::Dbo::ptr<Stock> stocks;
+
+		template<typename Action>
+		void persist(Action& a) {
+			Wt::Dbo::field(a, name, "name");
+			Wt::Dbo::field(a, date, "date");
+			Wt::Dbo::field(a, price, "price");
+			Wt::Dbo::belongsTo(a, stocks, "stock");
+		}
+	};
+
 //подключается к PostgreSQL;
 //создаёт все необходимые таблицы и связи между ними;
 //заполняет таблицы тестовыми данными из С++ кода.Данные для заполнения выберите произвольно;
@@ -44,16 +104,19 @@ int main() {
 		
 		
 		s.setConnection(move(connection));
-		s.mapClass<user>("user");
+		s.mapClass<Publisher>("publisher");
+		s.mapClass<Book>("book");
+		s.mapClass<Sale>("sale");
+		s.mapClass<Shop>("shop");
+		s.mapClass<Stock>("stock");
+		
+		s.createTables();
 		Wt::Dbo::Transaction t(s);
-		
-		
-		//s.createTables();
 
 
 
-		std::unique_ptr<user> joe(new user);
-		joe->name = "Joe";
+		std::unique_ptr<Publisher> joe(new Publisher{ "Roga", {} });
+		joe-> = "Joe";
 		joe->phone = "12345";
 		joe->karma = 100;
 		Wt::Dbo::ptr<user> joeDb = s.add(std::move(joe));
