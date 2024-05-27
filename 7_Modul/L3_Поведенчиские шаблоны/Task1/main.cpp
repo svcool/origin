ec9ff2ffda8e584ffa179329acdf3abff312401f
Myrep <sergyar@list.ru>
2024-05-27 23:54:28 +1000

    1

--- 7_Modul/L3_Поведенчиские шаблоны/Task1/main.cpp
+++ 7_Modul/L3_Поведенчиские шаблоны/Task1/main.cpp
@@ -1,1 +1,15 @@
-﻿
+﻿#include <iostream>
+
+class LogCommand {
+public:
+    virtual ~LogCommand() = default;
+    virtual void print(const std::string& message) = 0;
+};
+
+void print(LogCommand&);
+
+int main() {
+
+
+}
+
