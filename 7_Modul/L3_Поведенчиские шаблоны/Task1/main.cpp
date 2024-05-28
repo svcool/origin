51eb23ecfad0e1043b45263eed136afa41735412
Myrep <sergyar@list.ru>
2024-05-29 00:05:43 +1000

    1

--- 7_Modul/L3_Поведенчиские шаблоны/Task1/main.cpp
+++ 7_Modul/L3_Поведенчиские шаблоны/Task1/main.cpp
@@ -1,4 +1,6 @@
 ﻿#include <iostream>
+#include <fstream>
+#include <string>
 
 class LogCommand {
 public:
@@ -8,8 +10,43 @@ public:
 
 void print(LogCommand&);
 
-int main() {
+class LogToFileCommand : LogCommand {
+private:
+    std::string message;
+    std::string filePath;
+public:
+public:
+    LogToFileCommand(const std::string& msg, const std::string& path) : message(msg), filePath(path) {}
 
+    void print(Logger& logger) override {
+        logger.logToFile(message, filePath);
+    }
+};
+
+class LogPrintCommand : LogCommand {
+private:
+    std::string message;
+public:
+    LogPrintCommand(const std::string& msg) : message(msg) {}
+    void print(Logger& logger) override {
+        logger.logError(message);
+    }
+};
+
+
+
+// Function to execute a command
+void print(LogCommand& command) {
+    command.print();
+}
+
+
+int main() {
+    // Create command objects
+    Command* infoCommand = new LogInfoCommand("This is an information message");
+    Command* errorCommand = new LogErrorCommand("This is an error message");
+    Command* fileCommand = new LogToFileCommand("This message is written to file", "log.txt");
+    LogCommand log("This is a log message");
 
 }
 
