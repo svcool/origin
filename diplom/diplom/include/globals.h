981afaf733c278212051b4b6d2fc42ea45dde1f7
Myrep <sergyar@list.ru>
2024-11-21 00:14:05 +1000

    1

---
+++ diplom/diplom/include/globals.h
@@ -0,0 +1,10 @@
+// GLOBAL_VARIABLES_H
+#ifndef GLOBAL_VARIABLES_H
+#define GLOBAL_VARIABLES_H
+
+#include <iostream>
+#include <Windows.h>
+#include <bd.h>
+
+
+#endif // GLOBAL_VARIABLES_H
---
+++ diplom/diplom/src/main.cpp
@@ -0,0 +1,46 @@
+﻿#include <globals.h>
+
+
+using namespace std;
+#pragma execution_character_set("utf-8")
+
+int main()
+{
+	//setlocale(LC_ALL, "Russian");
+	SetConsoleCP(CP_UTF8);
+	SetConsoleOutputCP(CP_UTF8);
+	//setvbuf(stdout, nullptr, _IOFBF, 10000);
+	try {
+		//создаем базу данных
+		clientdb r("db_words");
+		manage_db db;
+		//создаем таблицы клиентов и телефонов
+		db.createTable("client", "phone");
+		db.add_client("Иван", "Иванов", "ivan@mail.ru", "1-12345678");
+		db.add_client("Петр", "Петров", "petr@mail.ru", "");
+		db.add_client("Макар", "Макаров", "makar@mail.ru", "111-12345678");
+		db.add_client("Антон", "Антонов", "anton@mail.ru", "1111-12345678");
+		//добавление телефона
+		db.add_phone("Иван", "Иванов", "1-112345678");
+		db.add_phone("Макар", "Макаров", "111-112345678");
+		//удаление телефона
+		db.delete_phone("Макар", "Макаров", "111-12345678");
+		//удаление клиента
+		db.delete_client("Иван", "Иванов");
+		//изменение клиента
+		db.change_client("Макар", "Макаров", "Cергей", "Макаров", "sergey@mail.ru", "14353453");
+		// поиск клиентов по параметру
+		db.out_client("surname", "Макаров");
+		db.out_client("phone", "14353453");
+		db.out_client("name", "Петр");
+		db.out_client("email", "petr@mail.ru");
+		db.out_client("email", "pe3333333r@mail.ru");
+	}
+	//catch (pqxx::sql_error e) {
+	catch (const std::exception& e) {
+		cout << e.what() << "Исключение" << endl;
+	}
+
+	return 0;
+
+}
